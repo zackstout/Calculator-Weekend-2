4b7517334647c374d4d0f1befe027f79b826dfa6
Zack Stout <zackstout@gmail.com>
2017-10-14 22:41:06 -0500

    getting closer on decimal functionality but some strange outcomes are arising

--- server/modules/calculate.js
+++ server/modules/calculate.js
@@ -1,6 +1,6 @@
 function calculateIt(input) {
-  var x = parseInt(input.x);
-  var y = parseInt(input.y);
+  var x = parseForDec(input.x);
+  var y = parseForDec(input.y);
   console.log(input);
 
   var output = '';
@@ -19,3 +19,15 @@ function calculateIt(input) {
 }
 
 module.exports = calculateIt;
+
+function parseForDec(num) {
+  if (num.includes('.')) {
+    var first = num.slice(0, num.indexOf('.'));
+    var last = num.slice(num.indexOf('.') + 1);
+    var power = (last.length * (-1));
+    var out = parseInt(first) + Math.pow(10, power);
+    return out;
+  } else {
+    return parseInt(num);
+  }
+}
